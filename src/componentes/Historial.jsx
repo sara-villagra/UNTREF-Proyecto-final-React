a28c98936d7a68022c23b08a43e8a50805a7d8b6
sarahvill <130417177+sara-villagra@users.noreply.github.com>
2025-12-12 23:30:43 -0300

    Add button delete item historial

--- src/componentes/Historial.jsx
+++ src/componentes/Historial.jsx
@@ -5,7 +5,15 @@ import HistoryStyle from "../style/Historial.module.css";
 const Historial = () => {
   const { historial, setHistorial } = useContext(DataContext);
   const navigation = useNavigate();
-  const borrar = (e) => {
+  
+
+  const borrarUno = (id) => {
+  setHistorial((prevHistorial) =>
+    prevHistorial.filter((item) => item.id !== id)
+  );
+};
+
+  const borrarTodo = (e) => {
       setHistorial([]);
       return navigation("/historial")}
 
@@ -15,10 +23,13 @@ const Historial = () => {
     <>
       <section className={HistoryStyle.sectionHistory}>
         <h2 className={HistoryStyle.title}>{titulo}</h2>
+        {historial.length === 0 && (
+         <p>No hay consultas en el historial</p>
+         )}
 
         <ul className={HistoryStyle.ul}>
           {historial.map((item, indice) => (
-            <li className={HistoryStyle.lista} key={indice}>
+            <li className={HistoryStyle.lista} key={item.id}>
               <p>id: {item.id}</p>
               <p>Fecha: {item.fecha}</p>
               <p>Destino: {item.destino}</p>
@@ -26,12 +37,18 @@ const Historial = () => {
               <p>Incluye: {item.incluye}</p>
               <p>Precio:$ {item.precio}</p>
               <p>Valor:$ {item.valor}</p>
-              </li>
+              <button
+                type="button"
+                onClick={() => borrarUno(item.id)}
+                >
+               Eliminar
+              </button>
+            </li>
 
           ))}
           <form className={HistoryStyle.form} action="">
-            <button type="button" onClick={borrar}>
-              borrar
+            <button type="button" onClick={borrarTodo}>
+              Borrar Todo
             </button>
             <button type="button" onClick={() => navigation("/")}>
               Volver
